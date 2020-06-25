96d50f4df5317526767dba085a0745d6bcc889f5
arkiver <arkiver@protonmail.com>
2020-06-25 19:40:03 -0400

    initial

---
+++ warriorbot.lua
@@ -0,0 +1,135 @@
+local item_value = os.getenv('item_value')
+local item_type = os.getenv('item_type')
+local item_dir = os.getenv('item_dir')
+local warc_file_base = os.getenv('warc_file_base')
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local ignore_patterns = {}
+local addedtolist = {}
+local abortgrab = false
+
+local ids = {}
+
+for ignore in io.open("ignore-patterns", "r"):lines() do
+  ignore_patterns[ignore] = true
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+allowed = function(url, parenturl)
+  for pattern, _ in pairs(ignore_patterns) do
+    if string.match(url, pattern) then
+      return false
+    end
+  end
+
+  local tested = {}
+  for s in string.gmatch(url, "([^/]+)") do
+    if tested[s] == nil then
+      tested[s] = 0
+    end
+    if tested[s] == 6 then
+      return false
+    end
+    tested[s] = tested[s] + 1
+  end
+
+  return true
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+
+  if downloaded[url] ~= true and addedtolist[url] ~= true
+    and allowed(url, parent["url"]) then
+    addedtolist[url] = true
+    return true
+  end
+  
+  return false
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. "  \n")
+  io.stdout:flush()
+
+  if status_code >= 300 and status_code <= 399 then
+    local newloc = string.match(http_stat["newloc"], "^([^#]+)")
+    if string.match(newloc, "^//") then
+      newloc = string.match(url["url"], "^(https?:)") .. string.match(newloc, "^//(.+)")
+    elseif string.match(newloc, "^/") then
+      newloc = string.match(url["url"], "^(https?://[^/]+)") .. newloc
+    elseif not string.match(newloc, "^https?://") then
+      newloc = string.match(url["url"], "^(https?://.+/)") .. newloc
+    end
+    if downloaded[newloc] == true
+      or addedtolist[newloc] == true
+      or not allowed(newloc, url["url"]) then
+      tries = 0
+      return wget.actions.EXIT
+    end
+  end
+  
+  if status_code >= 200 and status_code <= 399 then
+    downloaded[url["url"]] = true
+    downloaded[string.gsub(url["url"], "https?://", "http://")] = true
+  end
+
+  if abortgrab == true then
+    io.stdout:write("ABORTING...\n")
+    return wget.actions.ABORT
+  end
+  
+  if status_code >= 500
+      or (status_code >= 400 and status_code ~= 404 and status_code ~= 405)
+      or status_code  == 0 then
+    io.stdout:write("Server returned "..http_stat.statcode.." ("..err.."). Sleeping.\n")
+    io.stdout:flush()
+    if tries > 6 then
+      io.stdout:write("\nI give up...\n")
+      io.stdout:flush()
+      tries = 0
+      if allowed(url["url"], nil) then
+        return wget.actions.ABORT
+      else
+        return wget.actions.EXIT
+      end
+    else
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+      return wget.actions.CONTINUE
+    end
+  end
+
+  tries = 0
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if abortgrab == true then
+    return wget.exits.IO_FAIL
+  end
+  return exit_status
+end
